7203774b8340bfdf7831f6ed92300c7c4ef97746
Shanti Stewart <shantiystewart@gmail.com>
2022-07-22 02:00:35 -0700

    Added config of SampleCNN input size tuning.

---
+++ experiments/sampleCNN_input_size_tuning/pool_size=2/n_blocks=13/first_conv_size=5/version_0/mus_pretrain.py
@@ -0,0 +1,21 @@
+"""Script to perform music (audio) pretraining."""
+
+
+from vcmr.utils.mus_pretrain_function import mus_pretrain
+
+
+if __name__ == "__main__":
+
+    # config file:
+    config_file = "experiments/sampleCNN_input_size_tuning/pool_size=2/n_blocks=13/first_conv_size=5/version_0/config_mus.yaml"
+    # log directory naming:
+    exp_name = "sampleCNN_input_size_tuning"
+    exp_run_name = "pool_size=2/n_blocks=13/first_conv_size=5/version_0"
+    # verbosity:
+    verbose = 1
+
+    # perform music pretraining:
+    print("\n")
+    mus_pretrain(config_file=config_file, exp_name=exp_name, exp_run_name=exp_run_name, verbose=verbose)
+    print("\n\n")
+
