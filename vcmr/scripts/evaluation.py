fe8ae00c51fb16ab4ff4b812670246f19692ef5d
klean2050 <k.avramidis@windowslive.com>
2022-08-27 14:44:17 -0700

    enhanced evaluation script with graphs

--- vcmr/scripts/evaluation.py
+++ vcmr/scripts/evaluation.py
@@ -1,25 +1,13 @@
-import argparse, pytorch_lightning as pl
-import matplotlib.pyplot as plt, pickle
+import argparse, pytorch_lightning as pl, warnings
+import matplotlib.pyplot as plt, pickle, numpy as np
 from pytorch_lightning import Trainer
-from torchaudio_augmentations import RandomResizedCrop
+warnings.filterwarnings("ignore")
 
 from vcmr.loaders import get_dataset, Contrastive
 from vcmr.models import SampleCNN
-from vcmr.trainers import ContrastiveLearning, SupervisedLearning
-from vcmr.utils import yaml_config_hook, evaluate, visualize
+from vcmr.trainers import SupervisedLearning
+from vcmr.utils import *
 
-
-def save_marginal(dct, name):
-    plt.figure(figsize=(25, 8), dpi=200)
-    plt.rcParams["font.size"] = 12
-    new_dct = dct.copy()
-    for i, key in enumerate(dct):
-        if i > 9 and i < len(dct.keys()) - 10:
-            del new_dct[key]
-    plt.bar(*zip(*new_dct.items()))
-    plt.grid(axis="y")
-    plt.title("PR-AUC" if name=="prs" else "ROC-AUC")
-    plt.savefig(f"data/{args.dataset}_{name}.png")
 
 if __name__ == "__main__":
 
@@ -35,48 +23,63 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     pl.seed_everything(args.seed)
-    train_dataset = get_dataset(args.dataset, args.dataset_dir, subset="train")
 
+    dataset = get_dataset(args.dataset, args.dataset_dir, subset="test", sr=args.sample_rate)
+    test_dataset = Contrastive(dataset, input_shape=(1, args.audio_length))
+    encoder = SampleCNN(strides=[3, 3, 3, 3, 3, 3, 3, 3, 3])
+    
     # ----------
     # EVALUATION
     # ----------
-    encoder = SampleCNN(strides=[3, 3, 3, 3, 3, 3, 3, 3, 3])
-    checkpoint = f"runs/VCMR-{args.dataset}/" + args.checkpoint_eval
-    module = SupervisedLearning(
-        args, encoder, output_dim=train_dataset.n_classes
+    
+    # music model checkpoint
+    checkpoint = f"runs/VCMR-{args.dataset}/" + args.checkpoint_mus
+    module_mus = SupervisedLearning(
+        args, encoder, output_dim=dataset.n_classes
     ).load_from_checkpoint(
-        checkpoint, encoder=encoder, output_dim=train_dataset.n_classes
+        checkpoint, encoder=encoder, output_dim=dataset.n_classes
+    )
+    evaluate(
+        module_mus,
+        test_dataset,
+        args.dataset + "_mus",
+        args.audio_length,
+        device=f"cuda:{args.n_cuda}"
     )
 
-    contrastive_test_dataset = Contrastive(
-        get_dataset(args.dataset, args.dataset_dir, subset="test", sr=args.sample_rate),
-        input_shape=(1, args.audio_length),
-        transform=RandomResizedCrop(n_samples=args.audio_length)
+    # music+video model checkpoint
+    checkpoint = f"runs/VCMR-{args.dataset}/" + args.checkpoint_vid
+    module_vid = SupervisedLearning(
+        args, encoder, output_dim=dataset.n_classes
+    ).load_from_checkpoint(
+        checkpoint, encoder=encoder, output_dim=dataset.n_classes
+    )
+    evaluate(
+        module_vid,
+        test_dataset,
+        args.dataset + "_vid",
+        args.audio_length,
+        device=f"cuda:{args.n_cuda}"
     )
     
-    all, _, fts, lbs = evaluate(
-        module,
-        contrastive_test_dataset,
-        args.dataset,
-        args.audio_length,
-        device="cuda"
-    )
-    print(all)
+    with open(f"data/{args.dataset}_mus_classes_dict.pickle", "rb") as fp:
+        a = pickle.load(fp)
+        a0 = {k: v[0] for k, v in a.items()}
+        a1 = {k: v[1] for k, v in a.items()}
 
-    # ------
-    # GRAPHS
-    # ------
-    with open(f"data/{args.dataset}_classes_dict.pickle", "rb") as fp:
-        dct = pickle.load(fp)
-    dct0 = {k: v[0] for k, v in dct.items()}
-    save_marginal(
-        {k: v for k, v in sorted(dct0.items(), key=lambda i: i[1])},
-        name="prs"
-    )
-    dct1 = {k: v[1] for k, v in dct.items()}
-    save_marginal(
-        {k: v for k, v in sorted(dct1.items(), key=lambda i: i[1])},
-        name="rcs"
-    )
-    # make t-SNE visuals
-    visualize(args.dataset, fts, lbs, name="test.png")
+    with open(f"data/{args.dataset}_vid_classes_dict.pickle", "rb") as fp:
+        b = pickle.load(fp)
+        b0 = {k: v[0] for k, v in b.items()}
+        b1 = {k: v[1] for k, v in b.items()}
+
+    _ = make_graphs(args.dataset, a0, b0, name="prs")
+    tops = make_graphs(args.dataset, a1, b1, name="rcs")
+
+    indices = [i for i, k in enumerate(b.keys()) if k in tops]
+    lbs = np.load(f"data/{args.dataset}_mus_lbs.npy")
+    lbs = np.stack([v[indices] for v in lbs])
+
+    fts1 = np.load(f"data/{args.dataset}_mus_fts.npy")
+    fts2 = np.load(f"data/{args.dataset}_vid_fts.npy")
+    visualize(args.dataset, fts1, lbs, name="test_mus.png")
+    visualize(args.dataset, fts2, lbs, name="test_vid.png")
