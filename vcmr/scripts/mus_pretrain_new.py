017328c24d5b2028820ef934e5d1409f2a48d062
Shanti Stewart <shantiystewart@gmail.com>
2022-09-02 19:22:36 -0700

    Ported code from mus_pretrain_function.py to mus_pretrain_new.py

--- vcmr/scripts/mus_pretrain_new.py
+++ vcmr/scripts/mus_pretrain_new.py
@@ -3,7 +3,6 @@
 
 import os
 import argparse
-from typing import List
 import pytorch_lightning as pl
 from pytorch_lightning import Trainer
 from pytorch_lightning.loggers import TensorBoardLogger
@@ -17,29 +16,21 @@ from vcmr.models.sample_cnn_config import SampleCNN
 from vcmr.trainers.contrastive_learning import ContrastiveLearning
 
 
-# path of (overall) directory in which to save training logs:
-LOG_DIR = "runs/"
+# script options:
+config_file = "SQuEm/configs/config_triplet_ProjectorLSTM.yaml"
+model_summary_info = ["input_size", "output_size", "num_params"]
+verbose = 1
+
 # constants:
-NUM_AUG_SAMPLES = 2
-AUDIO_CHUNK_LENGTH_SEC = 15
+NUM_AUG_SAMPLES = 2     # don't change this
 
 
-def mus_pretrain(config_file: str, exp_name: str, exp_run_name : str = None, model_summary_info : List[str] = ["input_size", "output_size", "num_params"], verbose : bool = 1) -> None:
-    """Performs music (audio) pretraining.
+if __name__ == "__main__":
+    print("\n\n")
 
-    Args:
-        config_file: Path of config (yaml) file.
-        exp_name: Name of experiment (used to name logs subdirectory).
-        exp_run_name: Name of experiment run (used to name logs subdirectory).
-        model_summary_info: What information to include in model summary.
-        verbose: Level of information to display.
-    
-    Returns: None
-    """
-
-    # -----------
-    # ARGS PARSER
-    # -----------
+    # --------------
+    # CONFIGS PARSER
+    # --------------
 
     # create args parser and link to PyTorch Lightning trainer:
     parser = argparse.ArgumentParser(description="VCMR")
@@ -54,10 +45,10 @@ def mus_pretrain(config_file: str, exp_name: str, exp_run_name : str = None, mod
     # MAYBE???:
     # args = Trainer.parse_argparser(parser.parse_args(""))
 
-    # set random seed:
-    print()
-    pl.seed_everything(args.seed)
-
+    # set random seed if selected:
+    if args.seed:
+        pl.seed_everything(args.seed, workers=True)
+    
 
     # -------------------
     # AUDIO AUGMENTATIONS
@@ -80,38 +71,60 @@ def mus_pretrain(config_file: str, exp_name: str, exp_run_name : str = None, mod
     # DATA LOADERS
     # ------------
 
-    if verbose > 0:
-        print("\nSetting up data loaders...")
+    if verbose:
+        print("\nSetting up dataset and data loaders...")
     
     # get training/validation datasets:
-    train_dataset = get_dataset("audio", args.dataset_dir, subset="train", sr=args.sample_rate)
-    valid_dataset = get_dataset("audio", args.dataset_dir, subset="valid", sr=args.sample_rate)
+    train_dataset = get_dataset(
+        "audio",
+        args.dataset_dir,
+        subset="train",
+        sr=args.sample_rate
+    )
+    valid_dataset = get_dataset(
+        "audio",
+        args.dataset_dir,
+        subset="valid",
+        sr=args.sample_rate
+    )
 
     # set up contrastive learning training/validation datasets:
     contrastive_train_dataset = Contrastive(
         train_dataset,
-        input_shape=(1, args.sample_rate * AUDIO_CHUNK_LENGTH_SEC),
+        input_shape=(1, args.sample_rate * args.audio_chunk_length_sec),
         transform=ComposeMany(train_transform, num_augmented_samples=NUM_AUG_SAMPLES)
     )
     contrastive_valid_dataset = Contrastive(
         valid_dataset,
-        input_shape=(1, args.sample_rate * AUDIO_CHUNK_LENGTH_SEC),
+        input_shape=(1, args.sample_rate * args.audio_chunk_length_sec),
         transform=ComposeMany(train_transform, num_augmented_samples=NUM_AUG_SAMPLES)
     )
 
-    # create training/validation PyTorch data loaders:
-    train_loader = DataLoader(contrastive_train_dataset, batch_size=args.batch_size, num_workers=args.workers, drop_last=True, shuffle=True)
-    valid_loader = DataLoader(contrastive_valid_dataset, batch_size=args.batch_size, num_workers=args.workers, drop_last=True, shuffle=False)
+    # create training/validation dataloaders:
+    train_loader = DataLoader(
+        contrastive_train_dataset,
+        batch_size=args.batch_size,
+        shuffle=True,
+        num_workers=args.workers,
+        drop_last=True
+    )
+    valid_loader = DataLoader(
+        contrastive_valid_dataset,
+        batch_size=args.batch_size,
+        shuffle=False,
+        num_workers=args.workers,
+        drop_last=True,
+    )
 
 
     # --------------
     # MODEL & LOGGER
     # --------------
 
-    if verbose > 0:
+    if verbose:
         print("\nSetting up model and logger...")
     
-    # backbone (audio) encoder:
+    # create backbone (audio) encoder:
     encoder = SampleCNN(
         n_blocks=args.n_blocks,
         n_channels=args.n_channels,
@@ -119,20 +132,24 @@ def mus_pretrain(config_file: str, exp_name: str, exp_run_name : str = None, mod
         conv_kernel_size=args.conv_kernel_size,
         pool_size=args.pool_size,
         activation=args.activation,
-        first_block_params={
-            "out_channels": args.first_out_channels,
-            "conv_size": args.first_conv_size
-        },
+        first_block_params=args.first_block_params,
         input_size=args.audio_length
     )
-    # full model:
-    model = ContrastiveLearning(args, encoder)
 
-    # logger (logs are saved to LOG_DIR/exp_name/exp_run_name/):
-    logger = TensorBoardLogger(LOG_DIR, name=exp_name, version=exp_run_name)
+    # create full model (LightningModule):
+    model = ContrastiveLearning(
+        args,
+        encoder
+    )
+
+    # create logger (logs are saved to /save_dir/name/version/):
+    logger = TensorBoardLogger(
+        save_dir=args.log_dir,
+        name=args.experiment_name,
+        version=args.experiment_version
+    )
 
 
-    # """
     # --------
     # TRAINING
     # --------
@@ -145,36 +162,54 @@ def mus_pretrain(config_file: str, exp_name: str, exp_run_name : str = None, mod
         args,
         logger=logger,
         max_epochs=args.m_epochs,
+        check_val_every_n_epoch=args.check_val_every_n_epoch,
+        log_every_n_steps=args.log_every_n_steps,
         sync_batchnorm=True,
-        log_every_n_steps=10,
-        check_val_every_n_epoch=1,
-        strategy="ddp_find_unused_parameters_false",
         accelerator="gpu",
-        devices="auto"
+        devices="auto",
+        strategy="ddp_find_unused_parameters_false"
+
+        # maybe set this for consistent training runs?:
+        # deterministic=True
     )
 
     # train model:
-    trainer.fit(model, train_loader, valid_loader)
-    # """
+    trainer.fit(
+        model,
+        train_dataloaders=train_loader,
+        val_dataloaders=valid_loader,
+        ckpt_path=args.ckpt_path
+    )
 
 
     # ---------------
     # MODEL SUMMARIES
     # ---------------
 
-    if verbose > 0:
-        print("\n\n\n\n\nCreating model summaries...")
+    if verbose:
+        if verbose >= 2:
+            print("\n\n")
+        print("\n\n\nCreating model summaries...")
     
-    # batch size of test tensor for model summaries (most likely just keep equal to 1):
-    batch_size_summary = 1
-
     # create model summaries directory:
-    summaries_dir = os.path.join(LOG_DIR, exp_name, exp_run_name, "model_summaries", "")
+    summaries_dir = os.path.join(args.log_dir, args.experiment_name, "model_summaries", "")
     os.makedirs(summaries_dir, exist_ok=True)
 
     # create model summaries:
-    encoder_summary = str(torchinfo.summary(model.encoder, input_size=(batch_size_summary, 1, args.audio_length), col_names=model_summary_info, depth=3, verbose=0))
-    projector_summary = str(torchinfo.summary(model.projector, input_size=(batch_size_summary, model.n_features), col_names=model_summary_info, depth=1, verbose=0))
+    encoder_summary = str(torchinfo.summary(
+        model.encoder,
+        input_size=(args.batch_size, 1, args.audio_length),
+        col_names=model_summary_info,
+        depth=3,
+        verbose=0
+    ))
+    projector_summary = str(torchinfo.summary(
+        model.projector,
+        input_size=(args.batch_size, model.n_features),
+        col_names=model_summary_info,
+        depth=1,
+        verbose=0
+    ))
     
     # save model summaries:
     encoder_summary_file = os.path.join(summaries_dir, "encoder_summary.txt")
@@ -184,10 +219,12 @@ def mus_pretrain(config_file: str, exp_name: str, exp_run_name : str = None, mod
     with open(projector_summary_file, "w") as text_file:
         text_file.write(projector_summary)
     
-    # display encoder summary, if selected:
+    # display model summaries, if selected:
     if verbose >= 2:
         print("\n\nENCODER SUMMARY:\n")
         print(encoder_summary)
         print("\n\n\n\nPROJECTOR SUMMARY:\n")
         print(projector_summary)
+    
+    print("\n\n")
 
