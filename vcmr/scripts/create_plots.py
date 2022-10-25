c3efc919ac3457e919cb1b53832d1004c4ab5fdc
Shanti Stewart <shantiystewart@gmail.com>
2022-10-24 20:22:07 -0700

    Updated create_plots.py

--- vcmr/scripts/create_plots.py
+++ vcmr/scripts/create_plots.py
@@ -17,6 +17,7 @@ verbose = 1
 # for plots by tag category:
 global_metric_files = ["results/final_evaluation/magnatagatune/global_metrics.json"]
 tag_categories = ["instrument", "genre", "mood", "vocals"]
+include_plot_title = False
 
 
 if __name__ == "__main__":
@@ -38,7 +39,7 @@ if __name__ == "__main__":
     # create tag-category plots directory:
     tag_category_plots_dir = os.path.join(plots_dir, "tag_categories", "")
     os.makedirs(tag_category_plots_dir, exist_ok=True)
-
+    
     # create tag-category plots:
     for i in range(len(global_metric_files)):
         # load global metrics dictionary:
@@ -49,11 +50,15 @@ if __name__ == "__main__":
 
         # loop over metric types:
         for metric_type in metric_types:
+            if include_plot_title:
+                plot_title = "{} by Tag Category for {} Dataset".format(metric_type, dataset_names[i])
+            else:
+                plot_title = None
             plot_tag_categories(
                 global_metrics,
                 metric_type=metric_type,
-                dataset_name=dataset_names[i],
                 tag_categories=tag_categories,
+                plot_title=plot_title,
                 save_path=os.path.join(tag_category_plots_dir, datasets[i], "tag_category_" + metric_type + ".png"),
                 fig_num=fig_num
             )
