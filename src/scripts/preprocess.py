aec17a18eaee90d4a783d97fa91f00a7a9ad8f26
klean2050 <k.avramidis@windowslive.com>
2023-03-21 22:01:40 -0700

    add quality check

--- src/scripts/preprocess.py
+++ src/scripts/preprocess.py
@@ -5,19 +5,34 @@ from sklearn.preprocessing import StandardScaler
 from tqdm import tqdm
 
 
+def which_days(prcp):
+    metadata = prcp.replace("ecg", "metadata")
+    metadata = pd.read_csv(metadata)
+    result = metadata.loc[metadata["rrCoverageRatio"] > 0.9, "record_id"]
+    return result.values
+
+
 def process_prcp(prcp):
 
     # load ECG of participant
     df = pd.read_csv(os.path.join(path, prcp))
-    # make datetime timestamp
-    try:
-        df["Timestamp"] = pd.to_datetime(df["Timestamp"])
-    except:
-        df["Timestamp"] = pd.to_datetime(df["timestamp"])
+
+    # get days with good coverage
+    days = which_days(os.path.join(path, prcp))
+    if not len(days):
+        return []
+
+    # filter out days with bad coverage
+    df = df.loc[df["record_id"].isin(days)]
+
+    # correct column notation
+    if "timestamp" in df.columns:
+        df["Timestamp"] = df["timestamp"]
         df = df.drop(columns=["timestamp", "record_id"])
 
-    # sort based on timestamp and get ECG
+    # sort based on Timestamp and get ECG
     df = df.set_index("Timestamp").sort_index()["raw_ecg"]
+    df.index = pd.to_datetime(df.index, utc=True)
 
     final_samples = []
     prcp_samples = [
@@ -38,12 +53,14 @@ def process_prcp(prcp):
 
 if __name__ == "__main__":
     path = "/home/kavra/Datasets/tiles-phase1-opendataset/omsignal/ecg/"
-    out_path = "data/"
+    out_path = "data_upd/"
 
     os.makedirs(out_path, exist_ok=True)
     for prcp in tqdm(os.listdir(path)):
+        print(prcp)
         if f"{prcp.split('.')[0]}.npy" in os.listdir(out_path):
             continue
         samples = process_prcp(prcp)
-        samples = StandardScaler().fit_transform(samples)
-        np.save(out_path + prcp.split('.')[0], samples)
+        if len(samples):
+            samples = StandardScaler().fit_transform(samples)
+            np.save(out_path + prcp.split(".")[0], samples)
