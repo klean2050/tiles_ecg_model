3b2982c84d61e77cd79bfcfa5521846f12d7e03f
klean2050 <k.avramidis@windowslive.com>
2023-06-16 07:54:31 -0700

    attention mechanism

--- src/trainers/supervised_learning.py
+++ src/trainers/supervised_learning.py
@@ -65,13 +65,14 @@ class SupervisedLearning(LightningModule):
             self.att_dim = int(num * self.hparams.projection_dim)
 
         # attention (Q, K, V) mechanism
-        self.attention = nn.Linear(self.att_dim, self.att_dim)
         self.query = nn.Linear(self.att_dim, self.att_dim)
         self.key = nn.Linear(self.att_dim, self.att_dim)
+        self.value = nn.Linear(self.att_dim, self.att_dim)
 
         # classification projector
         self.classifier = nn.Linear(self.att_dim, output_dim)
 
+        # helper modules
         self.validation_true = list()
         self.validation_pred = list()
 
@@ -80,23 +81,25 @@ class SupervisedLearning(LightningModule):
         all_vectors = []
         for m in self.modalities:
             if m != "skt":
-                x[m] = x[m].unsqueeze(1)
+                if self.train:
+                    x[m] = x[m].unsqueeze(1)
                 all_vectors.append(self.models[m](x[m]))
-
         if "skt" in self.modalities:
             all_vectors.append(x["skt"])
 
+        # multimodal fusion
         fused_vector = torch.cat(all_vectors, dim=-1)
-        if flag:
-            fused_vector = fused_vector.half()
-
-        # attention (Q, K, V) mechanism
+        fused_vector = fused_vector.half() if flag else fused_vector.float()
         fused_vector = fused_vector / fused_vector.norm(dim=1, keepdim=True)
-        attention_vector = self.attention(fused_vector)
-        weights = torch.matmul(self.query(attention_vector), self.key(fused_vector).T)
-        weights = (weights / attention_vector.shape[-1] ** 0.5).softmax(0)
-        fused_vector = torch.matmul(weights, fused_vector)
-        # print(weights)
+
+        # attention mechanism
+        q = self.query(fused_vector)
+        k = self.key(fused_vector)
+        v = self.value(fused_vector)
+
+        weights = torch.matmul(q, k.T)
+        weights = (weights / q.shape[-1] ** 0.5).softmax(0)
+        fused_vector = torch.matmul(weights, v)
 
         # extract cls predictions
         preds = self.classifier(fused_vector).squeeze()
@@ -117,7 +120,6 @@ class SupervisedLearning(LightningModule):
         preds, y = self.forward(x, y, True)
         loss = self.compute_loss(preds, y)
         self.log("Train/loss", loss, sync_dist=True, batch_size=self.bs)
-        return loss
 
     def validation_epoch_end(self, _):
         if "avec" in self.args.dataset_dir or "epic" in self.args.dataset_dir:
@@ -125,18 +127,12 @@ class SupervisedLearning(LightningModule):
                 torch.stack(self.validation_pred), torch.stack(self.validation_true)
             )
             ccc = mean_ccc(self.validation_pred, self.validation_true)
-            rmse = torch.sqrt(
-                self.mse(
-                    torch.stack(self.validation_pred), torch.stack(self.validation_true)
-                )
-            )
             self.log("Valid/ccc", ccc, sync_dist=True, batch_size=self.bs)
             self.log("Valid/cccloss", cccloss, sync_dist=True, batch_size=self.bs)
-            self.log("Valid/rmse", rmse, sync_dist=True, batch_size=self.bs)
 
             self.validation_true = list()
             self.validation_pred = list()
-
+        
     def validation_step(self, batch, _):
         data, y, _ = batch
         x = {key: data[key] for key in self.modalities}
@@ -147,21 +143,24 @@ class SupervisedLearning(LightningModule):
             auroc = roc_auc_score(y.cpu(), preds.cpu())
             preds = (torch.sigmoid(preds).detach() > 0.5) * 1
             f1 = f1_score(y.cpu(), preds.cpu(), average="macro", zero_division=0)
+
             self.log("Valid/f1", f1, sync_dist=True, batch_size=self.bs)
             self.log("Valid/auroc", auroc, sync_dist=True, batch_size=self.bs)
+
         elif "avec" in self.args.dataset_dir or "epic" in self.args.dataset_dir:
             for idx in range(len(preds.cpu())):
                 self.validation_pred.append(preds.cpu()[idx])
                 self.validation_true.append(y.cpu()[idx])
+
         else:
-            y, preds = y.long(), preds.argmax(dim=1)
+            preds = preds.argmax(dim=1)
             acc = accuracy_score(y.cpu(), preds.cpu())
             f1 = f1_score(y.cpu(), preds.cpu(), average="macro", zero_division=0)
+
             self.log("Valid/f1", f1, sync_dist=True, batch_size=self.bs)
             self.log("Valid/acc", acc, sync_dist=True, batch_size=self.bs)
 
         self.log("Valid/loss", loss, sync_dist=True, batch_size=self.bs)
-        return loss
 
     def configure_optimizers(self):
         optimizer = torch.optim.AdamW(
